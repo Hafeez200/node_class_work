30349157f531a01999e0c8d5888bebe5ba444d4f
Hafeez200 <saibhaji24@email.com>
2024-08-11 14:45:53 +0500

    done

--- index.mjs
+++ index.mjs
@@ -5,9 +5,9 @@ const server = createServer((req, res) => {
   res.end('This is first class\nHello World!\n Today we learn about modeuls , import, export etc ' );
 });
 
-// starts a simple http server locally on port 3000
+
 server.listen(3000, () => {
   console.log('Server is running/listening on port 3000');
 });
 
-// run with `node server.mjs`
+
