0bb5ce004ce59db8b6c6a1fb5ace5020917ddd20
Hafeez200 <saibhaji24@email.com>
2024-08-31 14:48:10 +0500

    class 2

--- index.mjs
+++ index.mjs
@@ -2,7 +2,23 @@
 import { createServer } from 'http';
 
 const server = createServer((req, res) => {
-  res.end('This is first class\nHello World!\n Today we learn about modeuls , import, export etc ' );
+  const url = req.url
+  if (url==="/"){
+    res.writeHead(200,{"Content-Type":"text/plain"})
+    res.end("This is main page")
+  }
+  else if(url==="/home"){
+    res.writeHead(200,{"Content-Type":"text/plain"})
+    res.end("This is home page")
+  }
+  else if (url==="/about"){
+    res.writeHead(200,{"Content-Type":"text/plain"})
+    res.end("This is About page")
+  }
+  else{
+    res.writeHead(200,{"Content-Type":"text/plain"})
+    res.end("404 not found")
+  }
 });
 
 
